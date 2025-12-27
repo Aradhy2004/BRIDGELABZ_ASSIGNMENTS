60b98def01c0a5a2fa65c48c76bd2e5438af7ee0
Gaurish Aradhy <150017476+Aradhy2004@users.noreply.github.com>
2025-12-27 14:10:51 +0530

    Delete WEEK_1 directory

--- WEEK_1/basic core programs/harmonic_number.py
+++
@@ -1,8 +0,0 @@
-n=int(input("Enter a number: "))
-def harmonic(n):
-    if n == 0:
-        return 0
-    else:
-        return sum(1/n for n in range(1, n+1))
-result = harmonic(n)
-print(f"The {n}th harmonic number is: {result}")
