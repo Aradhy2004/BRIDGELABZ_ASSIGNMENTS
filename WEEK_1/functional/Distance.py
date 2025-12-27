5e2e1a99f94872d806db3448a860f3c5a7d003ad
Gaurish Aradhy <150017476+Aradhy2004@users.noreply.github.com>
2025-12-27 14:10:14 +0530

    Delete WEEK_1 directory

--- WEEK_1/functional/Distance.py
+++
@@ -1,5 +0,0 @@
-x = int(input("enter x coordinate: "))
-y = int(input("enter y coordinate: "))
-
-distance = ((x * x) + (y * y)) ** 0.5
-print("distance from (", x, "," , y, ") to the origin is ", distance)
